f8b07db3745f0bfa846510e56a069c4ded3dde78
UliyaSnisar <66842163+UliyaSnisar@users.noreply.github.com>
2021-05-10 15:33:24 +0300

    adds hero section styles

---
+++ src/js/appendHero.js
@@ -0,0 +1,9 @@
+import hero from '../templates/hero.hbs';
+
+const heroContainer = document.querySelector('.hero');
+
+function heroAppend() {
+    heroContainer.insertAdjacentHTML('beforeend', hero());
+    console.log(hero);
+};
+heroAppend();
