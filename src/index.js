13b4e3f7d60cc4c8e1be5af70bfccc465bd10b38
Dmitriy Nikolaiev <dimnik.u11@gmail.com>
2021-05-14 16:43:28 +0300

    Добавляет обработку формы и исправления

--- src/index.js
+++ src/index.js
@@ -35,11 +35,31 @@ eventList.searchEvents();
 //
 //
 
-// const searchInput = document.querySelector('#search-events-form');
-// searchInput.addEventListener('submit', event => {
-//   event.preventDefault();
-//   const searchValue = event.target.elements['event'].value.trim();
-//   const countryValue = event.target.elements['country'].value;
-//   // console.log(event.target.elements['country'], '---input-country');
-//   eventList.searchEvents(searchValue, countryValue);
-// });
+import countryCodes from './js/countryCodes';
+const selectRef = document.querySelector('#input-country');
+
+function createCountriesSelect() {
+  const options = countryCodes.map(({ alphaCode, name }) => {
+    // деструктуризація елементів масиву обєктів країн
+    return `<option value="${alphaCode}">${name}</option>`; //парент-строка з даними
+  });
+  selectRef.insertAdjacentHTML('beforeend', options.join('')); // заганяєм це все добро в кнопку
+}
+createCountriesSelect();
+
+const searchForm = document.querySelector('#search-events-form');
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const searchValue = event.target.elements['event'].value.trim();
+  const countryValue = event.target.elements['country'].value;
+  console.log(countryValue, '--countryValue');
+  // console.log(event.target.elements['country'], '---input-country');
+  eventList.searchEvents(searchValue, countryValue);
+});
+
+const modalRef = document.querySelector('.backdropEvent');
+const eventListRef = document.querySelector('.cards-holder');
+eventListRef.addEventListener('click', e => {
+  const cardRef = e.target.closest('.event-card');
+  console.log(cardRef.dataset.id);
+});
