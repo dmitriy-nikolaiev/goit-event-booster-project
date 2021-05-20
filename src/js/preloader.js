cdb9443bc0bca8740e94fcae1ba998dc9beab1d7
Dmitriy Nikolaiev <dimnik.u11@gmail.com>
2021-05-20 21:49:19 +0300

    Добавляет исправления

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -2,11 +2,11 @@ let preloaderRef = document.getElementById('preloader');
 
 window.addEventListener('load', closePreloader);
 function closePreloader() {
+  // setTimeout(() => {
+  document.body.classList.add('loaded_hiding');
   setTimeout(() => {
-    document.body.classList.add('loaded_hiding');
-    setTimeout(() => {
-      document.body.classList.add('loaded');
-      document.body.classList.remove('loaded_hiding');
-    }, 1000);
+    document.body.classList.add('loaded');
+    document.body.classList.remove('loaded_hiding');
   }, 1000);
+  // }, 1000);
 }
